eb95d33d074957172cbb6e1a3030962c54cc5b14
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-12-14 11:19:19 +0100

    fix person bug

--- custom_components/asterisk/www/sipjs-card.js
+++ custom_components/asterisk/www/sipjs-card.js
@@ -22435,7 +22435,7 @@
                         `;
                     }
                     var isMe = false;
-                    if (this.hass.user.name.toLowerCase() == this.hass.states[ent.person].name.toLowerCase()) {
+                    if (this.hass.user.name.toLowerCase() == this.hass.states[ent.person].id.toLowerCase()) {
                         isMe = true;
                     }
                     return lit_element__WEBPACK_IMPORTED_MODULE_0__.html `
@@ -22500,7 +22500,7 @@
             this.config.entities.map(ent => {
                 var extension = ent.entity.match(/\d/g).join("");
                 var person = this.hass.states[ent.person];
-                if (this.hass.user.name.toLowerCase() == person.name.toLowerCase()) {
+                if (this.hass.user.name.toLowerCase() == person.id.toLowerCase()) {
                     aor = "sip:" + extension + "@" + this.config.server;
                     authorizationUsername = extension;
                     authorizationPassword = ent.secret;
