a60fc97c08d2b83e421a8e8b20fab39ce5f975b7
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2021-12-19 21:21:29 +0100

    make video optional

--- custom_components/asterisk/www/sipjs-card.js
+++ custom_components/asterisk/www/sipjs-card.js
@@ -22418,7 +22418,7 @@
                   <h2 style="text-align: center; padding-top: 15px; margin-top: 0;" id="name">Idle</h2>
                   <span style="float:left" id="state">Connecting</span>
                   <span style="float:right" id="time">00:00</span>
-                  <video class="video" id="remoteVideo"></video>
+                  ${this.config.video ? '<video class="video" id="remoteVideo"></video>' : ''}
                   <br><hr style="margin-bottom: 12px;">
                   <mwc-button outlined @click=${() => this._answer()} style="float: right;">
                       <svg class="icon" viewBox="0 0 24 24">
@@ -22512,12 +22512,12 @@
                     authorizationPassword = ent.secret;
                 }
             });
-            const options = {
+            
+            var options = {
                 aor,
                 media: {
                     remote: {
                         audio: this.renderRoot.querySelector("#remoteAudio"),
-                        video: this.renderRoot.querySelector('#remoteVideo')
                     }
                 },
                 userAgentOptions: {
@@ -22525,6 +22525,10 @@
                     authorizationUsername,
                 }
             };
+
+            if (this.config.video) {
+                options.media.remote.video = this.renderRoot.querySelector('#remoteVideo');
+            }
 
             var port = "8089";
 
