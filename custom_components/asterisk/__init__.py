06fb0c6d7556a54a732aede2efda329f339aec4e
TECH Fox <32220029+TECH7Fox@users.noreply.github.com>
2022-05-22 22:12:58 +0200

    fix status bug

--- custom_components/asterisk/__init__.py
+++ custom_components/asterisk/__init__.py
@@ -68,13 +68,13 @@ def handle_asterisk_event(event, manager, hass, entry):
     if (event.get_header("Event") == "EndpointList"):
         device = {
             "extension": event.get_header("ObjectName"),
-            "status": event.get_header("Status"),
+            "status": event.get_header("State"),
             "tech": "PJSIP"
         }
     else:
         device = {
             "extension": event.get_header("ObjectName"),
-            "status": event.get_header("Status"),
+            "status": event.get_header("State"),
             "tech": event.get_header("Channeltype")
         }
 
