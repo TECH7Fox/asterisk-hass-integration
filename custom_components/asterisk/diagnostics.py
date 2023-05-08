d8cc5a3cf5f459fae8c5cbb62d02d6de547236ef
Jordy Kuhne <32220029+TECH7Fox@users.noreply.github.com>
2023-05-08 22:31:28 +0200

    Add auto_reconnect to diagnostics

--- custom_components/asterisk/diagnostics.py
+++ custom_components/asterisk/diagnostics.py
@@ -1,8 +1,8 @@
-from asterisk.ami import AMIClient
+from asterisk.ami import AMIClient, AutoReconnect
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import CLIENT, DOMAIN
+from .const import CLIENT, DOMAIN, AUTO_RECONNECT
 
 
 async def async_get_config_entry_diagnostics(
@@ -10,14 +10,17 @@ async def async_get_config_entry_diagnostics(
 ) -> dict[str, any]:
     """Return diagnostics for a config entry."""
     client: AMIClient = hass.data[DOMAIN][config_entry.entry_id][CLIENT]
+    auto_reconnect: AutoReconnect = hass.data[DOMAIN][config_entry.entry_id][AUTO_RECONNECT]
     return {
         "AMI Client": {
             "Address": client._address,
             "Port": client._port,
             "AMI version": client._ami_version,
-            "Timeout": client._timeout,
-            "Listeners count": client._listeners.count(),
-            "Action count": client._action_counter,
-            "Encoding error count": client.encoding_errors.count(),
-        }
+        },
+        "Auto Reconnect": {
+            "Alive": auto_reconnect.is_alive(),
+            "Delay": auto_reconnect.delay,
+            "Name": auto_reconnect.name,
+            "Deamon": auto_reconnect.daemon,
+        },
     }
